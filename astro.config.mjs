158b20804520071c669cd574754e6098f1342001
Andriy Kovalenko <andriyworkkovalenko@gmail.com>
2024-08-25 21:43:10 +0300

    якісь зміни

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx(), alpinejs()]
+  integrations: [react(), tailwind(), mdx(), alpinejs(), ]
 });
